107bb9b6f70a57dfc4062095b9caa4e4592d62f0
Pierre Hubert <pierre.git@communiquons.org>
2021-05-17 16:10:54 +0200

    Calcul de fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -7,10 +7,26 @@ public class Launcher {
         Scanner scanner = new Scanner(System.in);
 
         do {
-            String instruction = scanner.next();
+            String instruction = scanner.nextLine();
             if ("quit".equals(instruction))
                 break;
+
+            if ("fibo".equals(instruction)) {
+                System.out.println("Veuillez maintenant définir l'index de fibo que vous souhaitez obtenir :");
+                System.out.println(fibo(scanner.nextInt()));
+                scanner.nextLine();
+                continue;
+            }
+
             System.out.println("Unknown command");
         } while (true);
+    }
+
+    private static int fibo(int val) {
+        return switch (val) {
+            case 0 -> 0;
+            case 1 -> 1;
+            default -> fibo(val - 1) + fibo(val - 2);
+        };
     }
 }
